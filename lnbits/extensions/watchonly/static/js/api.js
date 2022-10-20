8bd9814598a0745f4030fbcd5d9094362b7ae815
Vlad Stan <stan.v.vlad@gmail.com>
2022-10-20 14:39:10 +0300

    feat: basic working example

--- lnbits/extensions/watchonly/static/js/api.js
+++ lnbits/extensions/watchonly/static/js/api.js
@@ -1,12 +1,23 @@
-console.log('### WatchOnly')
-window.LNbits.watchonly = {
-  api: {
-    getAccounts: function (wallet, network = 'Mainnet') {
-      return window.LNbits.api.request(
-        'get',
-        `/watchonly/api/v1/wallet?network=${network}`,
-        wallet.inkey
-      )
+function lnbitsWatchonlyJS(op) {
+  return {
+    api: {
+      request: function (method, url, apiKey, data) {
+        return axios({
+          method: method,
+          url: url,
+          headers: {
+            'X-Api-Key': apiKey
+          },
+          data: data
+        })
+      },
+      getAccounts: function (wallet, network = 'Mainnet') {
+        return this.request(
+          'get',
+          `/watchonly/api/v1/wallet?network=${network}`,
+          wallet.inkey
+        )
+      }
     }
   }
 }
