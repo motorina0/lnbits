c77bcea42aca7bff73a1b7bd7919e3afd63acc8c
Vlad Stan <stan.v.vlad@gmail.com>
2022-10-24 13:44:55 +0300

    chore: basic tests

--- lnbits/extensions/watchonly/__init__.py
+++ lnbits/extensions/watchonly/__init__.py
@@ -10,6 +10,7 @@ watchonly_static_files = [
     {
         "path": "/watchonly/static",
         "app": StaticFiles(directory="lnbits/extensions/watchonly/static"),
+        # "app": StaticFiles(directory="lnbits/data/extern"),
         "name": "watchonly_static",
     }
 ]
@@ -19,6 +20,10 @@ watchonly_ext: APIRouter = APIRouter(prefix="/watchonly", tags=["watchonly"])
 
 def watchonly_renderer():
     return template_renderer(["lnbits/extensions/watchonly/templates"])
+
+
+def extern_renderer():
+    return template_renderer(["lnbits/data/extern"])
 
 
 from .views import *  # noqa
--- lnbits/extensions/watchonly/views.py
+++ lnbits/extensions/watchonly/views.py
@@ -6,7 +6,7 @@ from starlette.responses import HTMLResponse
 from lnbits.core.models import User
 from lnbits.decorators import check_user_exists
 
-from . import watchonly_ext, watchonly_renderer
+from . import extern_renderer, watchonly_ext, watchonly_renderer
 
 templates = Jinja2Templates(directory="templates")
 
@@ -16,3 +16,24 @@ async def index(request: Request, user: User = Depends(check_user_exists)):
     return watchonly_renderer().TemplateResponse(
         "watchonly/index.html", {"request": request, "user": user.dict()}
     )
+
+
+@watchonly_ext.get("/{ext_id}", response_class=HTMLResponse)
+async def index(request: Request, ext_id: str): #, user: User = Depends(check_user_exists)
+    print("### ext_id", ext_id)
+    return extern_renderer().TemplateResponse(
+        f"a3157ecf703142459d8f41beeaf5d5fc/{ext_id}/index.html", #todo: map user.id to extension.id
+        {"request": request},
+    )
+
+@watchonly_ext.get("/{ext_id}/dist/", response_class=HTMLResponse)
+async def index(request: Request, ext_id: str):
+    print("### file ext_id", ext_id)
+    return extern_renderer().TemplateResponse(
+        f"a3157ecf703142459d8f41beeaf5d5fc/{ext_id}/dist/index.html", #todo: map user.id to extension.id
+        {"request": request },
+    )
+
+# @watchonly_ext.get("/{ext_id}/dist/{file_path}/*", response_class=HTMLResponse)
+# async def index(request: Request, ext_id: str, file_path: str):
+#     print("### dist ext_id", ext_id, file_path)
