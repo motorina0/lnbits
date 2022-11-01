f585eae050a6cdbce73f7334efba33fc231d0d53
Vlad Stan <stan.v.vlad@gmail.com>
2022-11-01 13:12:51 +0200

    feat: add api js

---
+++ lnbits/extensions/extern/static/js/api.js
@@ -0,0 +1,36 @@
+function externApiJS(op = {}) {
+  return {
+    hostname: op.hostname,
+    request: function (method, url, apiKey, data) {
+      return axios({
+        method: method,
+        url: toFullUrl(this.hostname, url),
+        headers: {
+          'X-Api-Key': apiKey
+        },
+        data: data
+      })
+    },
+
+    createResource: async function (wallet, extId, data, publicData) {
+      return this.request('post', '/extern/api/v1/resource', wallet.adminkey, {
+        ext_id: extId,
+        data: data,
+        public_data: publicData
+      })
+    },
+    getResources: function (wallet, extensionId) {
+      return this.request(
+        'get',
+        `/extern/api/v1/resources/${extensionId}`,
+        wallet.inkey
+      )
+    }
+  }
+
+  function toFullUrl(hostname = '', url = '') {
+    if (!hostname) return url
+    if (hostname.startsWith('localhost:')) return `http://${hostname}${url}`
+    return `https://${hostname}${url}`
+  }
+}
